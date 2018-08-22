c0fd13be4eed85f37bca6990fc006a62759b3928
duanyzhi <duanyzhi@outlook.com>
2018-08-22 15:32:16 +0800

    Add files via upload

---
+++ Climbing Stairs/main.py
@@ -0,0 +1,57 @@
+'''
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+对于本题,前提只有 一次 1阶或者2阶的跳法。
+
+a.如果两种跳法，1阶或者2阶，那么假定第一次跳的是一阶，那么剩下的是n-1个台阶，跳法是f(n-1);
+
+b.假定第一次跳的是2阶，那么剩下的是n-2个台阶，跳法是f(n-2)
+
+c.由a\b假设可以得出总跳法为: f(n) = f(n-1) + f(n-2)
+
+d.然后通过实际的情况可以得出：只有一阶的时候 f(1) = 1 ,只有两阶的时候可以有 f(2) = 2
+
+e.可以发现最终得出的是一个斐波那契数列：
+
+
+
+              | 1, (n=1)
+
+f(n) =     | 2, (n=2)
+
+              | f(n-1)+f(n-2) ,(n>2,n为整数)
+
+'''
+
+class Solution:
+    def jumpFloor(self, number):
+        # write code here
+        a = 1
+        b = 1
+        for i in range(number):
+            a,b = b,a+b
+        return a
+
+
+
+# ------------------------------------------------------------------------------
