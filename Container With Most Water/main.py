b90cd0bce36b7ad2322ab9b62199d64ca47fbad1
duanyzhi <duanyzhi@outlook.com>
2018-08-30 15:01:11 +0800

    Add files via upload

---
+++ Container With Most Water/main.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        res = 0
+        i = 0
+        j = len(height) - 1
+
+        while i < j:
+            res = max(res, min(height[i], height[j]) * (j - i))
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        return res
+
+
+# ------------------------------------------------------------------------------
